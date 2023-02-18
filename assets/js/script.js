56b3e59233427859ec1d3fef6a76bf9c4f61a73f
Dewey Watkins <deweywatkins77@gmail.com>
2023-02-18 18:34:12 -0500

    start function now displays first question and iterates through the answers to be displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,16 @@ var highscore = localStorage.getItem("highscore")
 var questionPTag = document.querySelector('.question p')
 var answersUlTag = document.querySelector('.answers ul')
 var timerEl = document.querySelector('.timercontainer p')
-var secondsLeft = 60
+var startBtn = document.getElementById('startBtn')
+var secondsLeft = 5
+var questionindex = 0
 
 var q0 = {
     question:'JavaScript is a ___ -side programming language.',
-    ans1:'Client',
-    ans2:'Server',
-    ans3:'Both',
-    ans4:'None',
+    1:'Client',
+    2:'Server',
+    3:'Both',
+    4:'None',
     correct:'Both'
 }
 var q1 = {
@@ -24,13 +26,47 @@ var q1 = {
 var questionsArray = [q0,q1]
 
 function startQuiz(){
+    //hide display button
+    startBtn.setAttribute("style", "display:none")
+    /*
+        Due to slight time delay need to display time left before starting.
+        This will also give user time to start reading the first question 
+        before timer starts
+    */
+    timerEl.textContent = "Seconds Remaining: " + secondsLeft
+    // display first question and start quiz timer
+    nextquestion()
     var timerInterval = setInterval(function() {
         timerEl.textContent = "Seconds Remaining: " + secondsLeft
         if(secondsLeft === 0) {
           // Stops execution of action at set interval
           clearInterval(timerInterval);
-          // Calls function to create and append image
+          //time run out and user lost
+          quitQuiz("lost")
         }
         secondsLeft--
       }, 1000);
 }
+
+function nextquestion(){
+    if (questionindex < questionsArray.length -1){
+        questionPTag.textContent = questionsArray[questionindex].question
+        // i is starting at one cause the fist answer does as well
+        for(i=1;i < 5; i++){
+            var li = document.createElement("li")
+            li.textContent = questionsArray[questionindex][i]
+            answersUlTag.appendChild(li)
+        }
+    }else{
+        //user finished quiz with time to spare and won
+        quitQuiz("won")
+    }
+    questionindex++
+}
+
+
+function quitQuiz(x){
+    console.log(x)
+}
+
+console.log()
